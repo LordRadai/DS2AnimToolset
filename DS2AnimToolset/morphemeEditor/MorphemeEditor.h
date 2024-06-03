01fd67302667fd541c849b6841556df3c86d80d5
Radai <lordradai@null.net>
2024-06-03 13:07:41 +0200

    Update MorphemeEditor.h

--- DS2AnimToolset/morphemeEditor/MorphemeEditor.h
+++ DS2AnimToolset/morphemeEditor/MorphemeEditor.h
@@ -1,6 +1,6 @@
 #pragma once
-#define APPNAME_W L"morphemeEditor 1.0.5b"
-#define APPNAME_A "morphemeEditor 1.0.5b"
+#define APPNAME_W L"morphemeEditor 1.1.0b"
+#define APPNAME_A "morphemeEditor 1.1.0b"
 
 #include "resources/resource.h"
 #include <d3d11.h>
