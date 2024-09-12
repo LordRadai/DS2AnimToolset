9636b9089726a5e6d5afad5ad37c707036ce2227
Radai <lordradai@null.net>
2024-09-12 18:07:08 +0200

    Delete DS2AnimToolset/RCore directory

--- DS2AnimToolset/RCore/RCore/src/RInput/RInput.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "RController/RController.h"
-
-namespace RInput
-{
-	bool isKeyHeld(int key);
-	bool isKeyStateChanged(int key);
-	bool isKeyComboPressed(int key1, int key2);
-}
