d9b1e75f353e2bd979384457882ba1b4add91e72
Radai <lordradai@null.net>
2024-09-18 18:54:03 +0200

    NodeTransitExport

---
+++ DS2AnimToolset/morphemeEditor/MorphemeSystem/MorphemeExport/NodeExport/TransitNodeExport.cpp
@@ -0,0 +1,13 @@
+#include "TransitNodeExport.h"
+#include "../../MRUtils/MRUtils.h"
+#include "NodeExport.h"
+#include "extern.h"
+#include "RCore.h"
+
+namespace MorphemeExport
+{
+	namespace NodeExport
+	{
+
+	}
+}
