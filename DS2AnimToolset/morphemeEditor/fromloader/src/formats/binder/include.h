58390b4240683104d23a5584b3184038f30ca585
Radai <lordradai@null.net>
2024-11-19 13:26:25 +0100

    Update include.h

--- DS2AnimToolset/morphemeEditor/fromloader/src/formats/binder/include.h
+++ DS2AnimToolset/morphemeEditor/fromloader/src/formats/binder/include.h
@@ -2,7 +2,7 @@
 #include "binder.h"
 //#include "bdf/bdf3.h" 
 //#include "bdf/bdf4.h" 
-#include "bnd/bnd3.h" 
+//#include "bnd/bnd3.h" 
 //#include "bnd/bnd4.h" 
 //#include "bhd/bhd5_0.h"
 //#include "bhd/bhd5_1.h"
