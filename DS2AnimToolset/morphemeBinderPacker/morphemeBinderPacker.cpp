93edf7701408053b0ce3f4abf86352389f237076
Radai <lordradai@null.net>
2024-11-23 18:41:48 +0100

    morpheemBinderPacker

--- DS2AnimToolset/morphemeBinderPacker/morphemeBinderPacker.cpp
+++ DS2AnimToolset/morphemeBinderPacker/morphemeBinderPacker.cpp
@@ -6,20 +6,90 @@ RLog* g_appLog;
 
 int getAnimIDByFilename(const std::wstring& filename) 
 {
-	std::wstring numericString;
+	std::wstring numericString = L"";
+	std::wstring animIdStr = filename.substr(1, 12);
 
-	for (char ch : filename) 
+	for (char ch : animIdStr)
 	{
 		if (std::isdigit(ch))
 			numericString += ch;
 	}
 
-	return !numericString.empty() ? std::stoi(numericString) : 0;
+	int animId = !numericString.empty() ? std::stoi(numericString) : 0;
+
+	g_appLog->debugMessage(MsgLevel_Debug, "animId=%d, filename=%ws\n", animId, filename.c_str());
+
+	return animId;
+}
+
+bool getExtAnimList(std::wstring path, std::unordered_map<int, int>& dataMap)
+{
+	g_appLog->debugMessage(MsgLevel_Info, "Loading extanibnd file list at %ws\n", path.c_str());
+
+	std::ifstream inputFile(path);
+
+	if (!inputFile) 
+	{
+		g_appLog->debugMessage(MsgLevel_Error, "Input file %ws does not exist\n", path.c_str());
+
+		return false;
+	}
+
+	std::string line;
+
+	int numLines = 0;
+	while (std::getline(inputFile, line)) 
+	{
+		std::istringstream lineStream(line);
+		std::string animIdStr, chrIdStr;
+
+		if (std::getline(lineStream, animIdStr, ',') && std::getline(lineStream, chrIdStr))
+		{
+			int animID = std::stoi(animIdStr);
+			int chrID = std::stoi(chrIdStr);
+
+			dataMap.insert(std::pair<int, int>(animID, chrID));
+		}
+
+		numLines++;
+	}
+
+	g_appLog->debugMessage(MsgLevel_Info, "Read %d entries from %ws\n", numLines, path.c_str());
+
+	inputFile.close();
+
+	for (const auto& [key, value] : dataMap)
+		g_appLog->debugMessage(MsgLevel_Debug, "\t(animId=%d, chrId=%d)\n", key, value);
+
+	return true;
+}
+
+void createWitchyBndXml(std::string filename, std::string binderFileName)
+{
+	tinyxml2::XMLDocument doc;
+	doc.NewDeclaration("version = \"1.0\" encoding=\"utf-8\"");
+
+	tinyxml2::XMLElement* root = doc.NewElement("anibnd4");
+
+	root->InsertNewChildElement("filename")->SetText(binderFileName.c_str());
+	root->InsertNewChildElement("compression")->SetText("DCX_DFLT_10000_24_9");
+	root->InsertNewChildElement("version")->SetText("10G29R1");
+	root->InsertNewChildElement("format")->SetText("IDs, Names1, Names2, Compression");
+	root->InsertNewChildElement("bigendian")->SetText("False");
+	root->InsertNewChildElement("bitbigendian")->SetText("False");
+	root->InsertNewChildElement("unicode")->SetText("True");
+	root->InsertNewChildElement("extended")->SetText("0x01");
+	root->InsertNewChildElement("unk04")->SetText("False");
+	root->InsertNewChildElement("unk05")->SetText("False");
+
+	doc.InsertEndChild(root);
+
+	doc.SaveFile(filename.c_str());
 }
 
 int main(int argc, char* argv[])
 {
-	g_appLog = new RLog(MsgLevel_Debug, "morphemeBinderPacker.log", "mcnMerger");
+	g_appLog = new RLog(MsgLevel_Debug, "morphemeBinderPacker.log", "morphemeBinderPacker");
 
 	if (argc < 2)
 	{
@@ -33,20 +103,28 @@ int main(int argc, char* argv[])
 
 	if (!std::filesystem::exists(runtimeBinary))
 	{
-		g_appLog->alertMessage(MsgLevel_Error, "Could not find folder %s", runtimeBinary.c_str());
+		g_appLog->alertMessage(MsgLevel_Error, "Could not find folder %ws", runtimeBinary.c_str());
 		return 1;
 	}
 
 	std::wstring chrIdStr = filepath.filename().wstring();
 	int chrId = std::stoi(chrIdStr.substr(1, 4));
+	std::unordered_map<int, int> animIdChrIdMap;
 
-	std::filesystem::path outputFolder = filepath.wstring() + L"\\morphemeBinders";
+	if (chrId == 1)
+		getExtAnimList(L"extanimlist.txt", animIdChrIdMap);
+
+	std::filesystem::path outputFolder = filepath.wstring() + L"\\binders";
 
 	for (const auto& entry : std::filesystem::directory_iterator(runtimeBinary)) 
 	{
 		if (entry.is_regular_file() && (entry.path().extension() == ".nsa" || entry.path().extension() == ".nmb")) 
 		{
 			std::wstring filename = entry.path().filename().wstring();
+			std::filesystem::path destinationFile = std::filesystem::path(outputFolder).wstring() + L"\\" + chrIdStr + L"\\runtimeBinary\\" + filename;
+			
+			CHAR binderFileName[256];
+			sprintf_s(binderFileName, "%ws.anibnd.dcx", chrIdStr.c_str());
 
 			if (entry.path().extension() == ".nsa")
 			{
@@ -54,13 +132,25 @@ int main(int argc, char* argv[])
 
 				if (chrId == 1)
 				{
+					int targetChrId = -1;
 
+					if (animIdChrIdMap.find(animId) != animIdChrIdMap.end())
+						targetChrId = animIdChrIdMap[animId];
+
+					if (targetChrId != -1)
+					{
+						wchar_t extAnibndFolderName[256];
+						wsprintf(extAnibndFolderName, L"c0001_%d", targetChrId);
+
+						destinationFile = std::filesystem::path(outputFolder).wstring() + L"\\" + chrIdStr + L"\\" + extAnibndFolderName + L"\\" + filename;
+					
+						sprintf_s(binderFileName, "%ws.extanibnd.dcx", extAnibndFolderName);
+					}
 				}
 			}
 
-
-			std::filesystem::path destinationFile = std::filesystem::path(outputFolder).wstring() + L"\\" + chrIdStr + L"\\" + filename;
 			std::filesystem::create_directories(destinationFile.parent_path());
+			createWitchyBndXml(destinationFile.parent_path().string() + "\\_witchy-anibnd4.xml", binderFileName);
 
 			std::filesystem::copy(entry.path(), destinationFile, std::filesystem::copy_options::overwrite_existing);
 
