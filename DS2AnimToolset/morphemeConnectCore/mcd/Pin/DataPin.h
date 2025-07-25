0b901aed3a4a990bd2ef3846f629efbfe3a45dbc
Radai <lordradai@null.net>
2025-07-25 21:36:22 +0200

    fix

--- DS2AnimToolset/morphemeConnectCore/mcd/Pin/DataPin.h
+++ DS2AnimToolset/morphemeConnectCore/mcd/Pin/DataPin.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Pin.h"
+#include "Pin.inl"
 #include "DataTypes.h"
 
 namespace mcd
--- DS2AnimToolset/morphemeConnectCore/mcd/Pin/Pin.cpp
+++
@@ -1,7 +0,0 @@
-#include "Pin.h"
-
-namespace mcd
-{
-	Pin::Pin(db::Node* parent, const std::string& name, const std::string pinName)
-		: db::Node(parent, name, pinName);
-}
