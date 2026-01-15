fa0900819c09173038dbacb02341f8b68971dd1a
Radai <lordradai@null.net>
2026-01-15 21:55:22 +0100

    Update PointerAttribute.cpp

--- DS2AnimToolset/NMDatabase/Attribute/SingleValue/PointerAttribute.cpp
+++ DS2AnimToolset/NMDatabase/Attribute/SingleValue/PointerAttribute.cpp
@@ -32,6 +32,7 @@ namespace db
 
 	bool PointerAttribute::writeValueXML(int format, SaverXML* saver)
 	{
+		// TODO: This is wrong
 		m_xmlElement->SetText(m_value->toDatabasePath().c_str());
 		return true;
 	}
