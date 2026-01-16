81f1ca73a14b2b6c18b62ff6eb923911f7cb8e22
Radai <lordradai@null.net>
2026-01-16 15:30:26 +0100

    Update StringAttribute.h

--- DS2AnimToolset/NMDatabase/Attribute/SingleValue/StringAttribute.h
+++ DS2AnimToolset/NMDatabase/Attribute/SingleValue/StringAttribute.h
@@ -31,6 +31,6 @@ namespace db
 		virtual NMP::Quat getValueAsQuaternion() const override { return NMP::QuatIdentity(); }
 
 		const std::string getValue() const { return getValueAsString(); }
-		void setValue(const std::string& value) { m_value = value; }
+		void setValue(const std::string value) { m_value = value; }
 	};
 }
