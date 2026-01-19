ebcaa458fedd105803e6f4e65e00f7e6d34ec9b2
Radai <lordradai@null.net>
2026-01-19 18:18:47 +0100

    Update MMDataPin.cpp

--- DS2AnimToolset/morphemeConnectCore/mcc/MorphemeManifest/MMManifestItem/Pin/MMDataPin.cpp
+++ DS2AnimToolset/morphemeConnectCore/mcc/MorphemeManifest/MMManifestItem/Pin/MMDataPin.cpp
@@ -6,7 +6,6 @@ namespace mcc
 	bool MMDataPin::addToGraphNode(mcd::GraphNode* node)
 	{
 		mcd::DataPin* pin = new mcd::DataPin(node, getPinName(), utils::getDataTypeFromString(this->getDataType()));
-		pin->setPassThroughEnabled(isPassThrough());
 		pin->setInput(isInput());
 
 		node->getPins()->add(pin);
