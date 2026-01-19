228aefde048dd73bec10fbef5847b4804a56d087
Radai <lordradai@null.net>
2026-01-19 16:24:32 +0100

    Update MMFunctionalPin.cpp

--- DS2AnimToolset/morphemeConnectCore/mcc/MorphemeManifest/MMManifestItem/Pin/MMFunctionalPin.cpp
+++ DS2AnimToolset/morphemeConnectCore/mcc/MorphemeManifest/MMManifestItem/Pin/MMFunctionalPin.cpp
@@ -40,7 +40,7 @@ namespace mcc
 	bool MMFunctionalPin::addToGraphNode(mcd::GraphNode* node)
 	{
 		mcd::FunctionalPin* pin = new mcd::FunctionalPin(node, getPinName());
-		pin->setInput(isInput());
+		pin->setIsInput(isInput());
 		pin->setPassThroughEnabled(isPassThrough());
 
 		for (size_t i = 0; i < this->m_requiredInterfaces.size(); i++)
