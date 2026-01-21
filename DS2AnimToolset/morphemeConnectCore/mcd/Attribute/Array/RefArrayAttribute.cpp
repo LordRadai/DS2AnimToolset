93c8046f57e4ba261a7ced63179f0681ee41e58b
Radai <lordradai@null.net>
2026-01-21 16:32:29 +0100

    Update RefArrayAttribute.cpp

--- DS2AnimToolset/morphemeConnectCore/mcd/Attribute/Array/RefArrayAttribute.cpp
+++ DS2AnimToolset/morphemeConnectCore/mcd/Attribute/Array/RefArrayAttribute.cpp
@@ -3,7 +3,7 @@
 namespace mcd
 {
 	RefArrayAttribute::RefArrayAttribute(db::CompositeAttribute* parent, std::string name)
-		: Attribute(parent, name, "Value"),
+		: Attribute(parent, "RefArrayAttribute", name),
 		m_valueAttr(std::make_unique<db::TypedAttributeArray<AttributePinNodeBase>>(this, "Value"))
 	{
 		this->addAttribute(m_valueAttr.get());
