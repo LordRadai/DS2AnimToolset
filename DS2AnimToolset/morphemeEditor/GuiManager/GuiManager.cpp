2eefbcc779aec37fd2943d42661cb8c9983af0c2
Radai <lordradai@null.net>
2026-01-29 17:59:12 +0100

    Update GuiManager.cpp

--- DS2AnimToolset/morphemeEditor/GuiManager/GuiManager.cpp
+++ DS2AnimToolset/morphemeEditor/GuiManager/GuiManager.cpp
@@ -660,7 +660,7 @@ void GuiManager::initGuiStyle()
 	colors[ImGuiCol_TabSelectedOverline] = ImVec4(0.73f, 0.11f, 0.11f, 1.00f);
 	colors[ImGuiCol_TabDimmed] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
 	colors[ImGuiCol_TabDimmedSelected] = ImVec4(0.27f, 0.27f, 0.27f, 1.00f);
-	colors[ImGuiCol_TabDimmedSelectedOverline] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	colors[ImGuiCol_TabDimmedSelectedOverline] = ImVec4(0.73f, 0.11f, 0.11f, 1.00f);
 	colors[ImGuiCol_DockingPreview] = ImVec4(0.26f, 0.59f, 0.98f, 0.70f);
 	colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
 	colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
