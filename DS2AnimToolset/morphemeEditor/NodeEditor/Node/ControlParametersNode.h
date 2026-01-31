e48626cf08e4073ea7ce3ce0d3c8d334cebb0b02
Radai <lordradai@null.net>
2026-01-31 19:19:21 +0100

    Update ControlParametersNode.h

--- DS2AnimToolset/morphemeEditor/NodeEditor/Node/ControlParametersNode.h
+++ DS2AnimToolset/morphemeEditor/NodeEditor/Node/ControlParametersNode.h
@@ -7,6 +7,7 @@ namespace NodeEditor
 	{
 	public:
 		ControlParametersNode(NodeEditor* editor, const std::string& name);
+
 		virtual ~ControlParametersNode() override {}
 		virtual void draw() override;
 		virtual void editorGUI() override {}
