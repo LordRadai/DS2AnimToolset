ff94e913d516b6dabef2f077fee78929d64aead7
Radai <lordradai@null.net>
2026-02-04 20:07:42 +0100

    Update NodeProcessor.h

--- DS2AnimToolset/morphemeEditor/MorphemeNetworkInspector/NodeProcessor/NodeProcessor.h
+++ DS2AnimToolset/morphemeEditor/MorphemeNetworkInspector/NodeProcessor/NodeProcessor.h
@@ -70,8 +70,10 @@ private:
 
 	void populateGraph(NodeEditor::Graph* graph, MR::NodeDef* ownerNodeDef);
 	void populateSubGraphs(NodeEditor::Graph* graph, MR::NodeDef* ownerNodeDef);
+
 	void processNodeConnectionsInBlendTree(NodeEditor::BlendTree* blendTree, std::vector<MR::NodeDef*>& childNodes);
 	void setBlendTreeLayout(NodeEditor::BlendTree* blendTree, std::vector<MR::NodeDef*>& childNodes);
+
 	void processNodeTransitionsInStateMachine(NodeEditor::StateMachine* stateMachine, MR::NodeDef* nodeDef);
 	void setStateMachineLayout(NodeEditor::StateMachine* stateMachine, MR::NodeDef* nodeDef);
 };
