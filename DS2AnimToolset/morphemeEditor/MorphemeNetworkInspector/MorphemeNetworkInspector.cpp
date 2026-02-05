486f02430ae7f62e506c06751e5b33248cb339a6
Radai <lordradai@null.net>
2026-02-05 19:14:27 +0100

    Update MorphemeNetworkInspector.cpp

--- DS2AnimToolset/morphemeEditor/MorphemeNetworkInspector/MorphemeNetworkInspector.cpp
+++ DS2AnimToolset/morphemeEditor/MorphemeNetworkInspector/MorphemeNetworkInspector.cpp
@@ -13,7 +13,7 @@ bool MorphemeNetworkInspector::loadNetwork(MR::NetworkDef* network)
 
 	if (network->getNumMultiplyConnectedNodes() != numControlParams)
 	{
-		g_appLog->debugMessage(MsgLevel_Error, "MorphemeNetworkInspector::loadNetwork: Networks with multiply connected nodes are not yet supported.");
+		g_appLog->alertMessage(MsgLevel_Info, "MorphemeNetworkInspector::loadNetwork: Networks with multiply connected nodes are not yet supported.");
 		return false;
 	}
 
